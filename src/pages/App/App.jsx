0b28187fd10181bc3497debdbefebaf8be070abb
angel melendez <angelmelen88@gmail.com>
2023-06-12 19:16:08 -0600

    instalacion de react router, y uso en App el routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,23 +1,39 @@
-import { Home } from '../home'
-import { MyAccount } from '../MyAccount'
-import { MyOrder } from '../MyOrder'
-import { MyOrders } from '../MyOrders'
-import { NotFound } from '../NotFound'
-import { SignIn } from '../SignIn'
-import './App.css'
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+
+import { Home } from '../home';
+import { MyAccount } from '../MyAccount';
+import { MyOrder } from '../MyOrder';
+import { MyOrders } from '../MyOrders';
+import { NotFound } from '../NotFound';
+import { SignIn } from '../SignIn';
+
+import './App.css';
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: '/', element: <Home/>},
+    {path: '/My-account', element: <MyAccount/>},
+    {path: '/My-order', element: <MyOrder/>},
+    {path: '/My-orders', element: <MyOrders/>},
+    {path: '/*', element: <NotFound/>},
+    {path: '/Sign-in', element: <SignIn/>},
+  ])
+  return routes
+}
 
 
-function App() {
+const App = () => { 
   return (
-    <>
-      <Home/>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  )
+}
+
+{/* <Home/>
       <MyAccount/>
       <MyOrder/>
       <MyOrders/>
       <NotFound/>
-      <SignIn/>
-    </>
-  )
-}
-
+      <SignIn/> */}
 export default App

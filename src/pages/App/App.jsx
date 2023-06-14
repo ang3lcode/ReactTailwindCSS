6fae3c300ddaac4a2fdcae80c23cb063dc714a8d
angel melendez <angelmelen88@gmail.com>
2023-06-13 19:23:55 -0600

    creacion del componente navbar

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,17 +6,18 @@ import { MyOrder } from '../MyOrder';
 import { MyOrders } from '../MyOrders';
 import { NotFound } from '../NotFound';
 import { SignIn } from '../SignIn';
+import { Navbar } from '../Components/Navbar/index'
 
 import './App.css';
 
 const AppRoutes = () => {
   let routes = useRoutes([
     {path: '/', element: <Home/>},
-    {path: '/My-account', element: <MyAccount/>},
-    {path: '/My-order', element: <MyOrder/>},
-    {path: '/My-orders', element: <MyOrders/>},
+    {path: '/my-account', element: <MyAccount/>},
+    {path: '/my-order', element: <MyOrder/>},
+    {path: '/my-orders', element: <MyOrders/>},
     {path: '/*', element: <NotFound/>},
-    {path: '/Sign-in', element: <SignIn/>},
+    {path: '/sign-in', element: <SignIn/>},
   ])
   return routes
 }
@@ -26,6 +27,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes/>
+      <Navbar/>
     </BrowserRouter>
   )
 }

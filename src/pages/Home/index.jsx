a0f6b2b9302c0a86f42216f70c8c8a0639cb0921
angel melendez <angelmelen88@gmail.com>
2023-06-17 14:53:43 -0600

    consumiendo lafakeStore API para la card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,27 @@
-import { Layout } from '../Components/Layout/index'
-import { Card } from '../Components/Card/index'
+import { Layout } from '../Components/Layout/index';
+import { Card } from '../Components/Card/index';
+import { useState,useEffect } from 'react';
 // import './'
 
 export function Home() {
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItem(data))
+  }, [])
   return (
     <>
       <Layout>
         Home
-        <Card/>
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            item?.map((item) => (
+              <Card key= {item.id} data={item} />            
+            ))
+          }
+        </div>
       </Layout>
     </>
   )
